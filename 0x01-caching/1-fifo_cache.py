2b8384839e6d88e75cff69721f9efb3045ca4db5
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2023-08-23 17:31:51 +0000

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,37 +1,48 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" FIFO Caching """
 
-'''Task 1: FIFO caching
-'''
-
-
-from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''A class `FIFOCache` that inherits from
-       `BaseCaching` and is a caching system.
-    '''
+    """ FIFO caching """
 
     def __init__(self):
+        """ Constructor """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.queue = []
 
     def put(self, key, item):
-        '''assign to the dictionary `self.cache_data` the
-           `item` value for the key `key`
-        '''
-
+        """ Puts item in cache """
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
 
         self.cache_data[key] = item
 
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
+
     def get(self, key):
-        '''return the value in `self.cache_data` linked to `key`
-        '''
+        """ Gets item from cache """
         return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        """ Moves element to last idx of list """
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get first element of list or None """
+        return array[0] if array else None
