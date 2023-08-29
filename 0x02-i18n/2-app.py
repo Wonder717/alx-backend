afac31bcc15f30144c846e56c6ca4233ada595fa
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2023-08-29 13:18:11 +0000

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python3
-'''Task 2: Get locale from request
+'''
+    Use Babel to get user locale.
 '''
 
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
-class Config:
-    '''Config class'''
-
-    DEBUG = True
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
+app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
+
+
+class Config(object):
+    '''
+        Babel configuration.
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        Render template for Babel usage.
+    '''
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''
+        Get user locale.
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
